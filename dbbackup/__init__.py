579ce4d982f2b8e2c202f5d9e85cc4dbce089ca3
Michael Shepanski <mjs7231@gmail.com>
2015-10-05 13:39:47 -0400

    Couldnt install from Pip without MANIFEST.in file; Upped version again for this

--- dbbackup/__init__.py
+++ dbbackup/__init__.py
@@ -1,6 +1,6 @@
 "Management commands to help backup and restore a project database to \
 AmazonS3, Dropbox or local disk."
-VERSION = __version__ = "2.3.0"
+VERSION = __version__ = "2.3.1"
 __author__ = 'Michael Shepanski'
 __email__ = 'mjs7231@gmail.com'
 __url__ = 'https://github.com/django-dbbackup/django-dbbackup'

b4215f326113571961e144d64333d44e570cb1f2
ZuluPro <montheanthony@hotmail.com>
2016-03-29 21:49:38 -0400

    Version 2.5.0

--- dbbackup/__init__.py
+++ dbbackup/__init__.py
@@ -1,6 +1,6 @@
 "Management commands to help backup and restore a project database to \
 AmazonS3, Dropbox or local disk."
-VERSION = (2, 4, 1)
+VERSION = (2, 5, 0)
 __version__ = '.'.join([str(i) for i in VERSION])
 __author__ = 'Michael Shepanski'
 __email__ = 'mjs7231@gmail.com'

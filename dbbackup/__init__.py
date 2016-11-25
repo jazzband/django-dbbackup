aae238a9d11c20105f03f9b185ed783c93de0d72
ZuluPro <montheanthony@hotmail.com>
2016-11-25 10:19:44 -0500

    Bump to 3.1.2

--- dbbackup/__init__.py
+++ dbbackup/__init__.py
@@ -1,5 +1,5 @@
 "Management commands to help backup and restore a project database and media"
-VERSION = (3, 1, 1)
+VERSION = (3, 1, 2)
 __version__ = '.'.join([str(i) for i in VERSION])
 __author__ = 'Michael Shepanski'
 __email__ = 'mjs7231@gmail.com'

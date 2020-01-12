8d74de2dd5457f866784813f2edb36085c817a8d
alex <devkral@web.de>
2020-01-12 19:59:40 +0100

    fix File invocation and doesn't depend on paramiko

--- dbbackup/db/base.py
+++ dbbackup/db/base.py
@@ -3,11 +3,9 @@ Base database connectors
 """
 import os
 import shlex
-from django.core.files.base import ContentFile
-from storages.backends.sftpstorage import SFTPStorageFile
-from storages.backends.ftp import FTPStorageFile
+from django.core.files.base import File
 from tempfile import SpooledTemporaryFile
-from subprocess import Popen, PIPE
+from subprocess import Popen
 from importlib import import_module
 from dbbackup import settings, utils
 from . import exceptions
@@ -138,9 +136,11 @@ class BaseCommandDBConnector(BaseDBConnector):
         full_env.update(self.env)
         full_env.update(env or {})
         try:
-            if isinstance(stdin, (ContentFile, SFTPStorageFile, FTPStorageFile)):
-                process = Popen(cmd, stdin=PIPE, stdout=stdout, stderr=stderr, env=full_env)
-                process.communicate(input=stdin.read())
+            if isinstance(stdin, File):
+                process = Popen(
+                    cmd, stdin=stdin.open("rb"), stdout=stdout, stderr=stderr,
+                    env=full_env
+                )
             else:
                 process = Popen(cmd, stdin=stdin, stdout=stdout, stderr=stderr, env=full_env)
             process.wait()

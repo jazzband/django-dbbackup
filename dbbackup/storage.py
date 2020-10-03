e155752694c8f2510af93f98c69944326fd7f35b
Marc LaBelle <mlabelle@acvauctions.com>
2020-10-03 09:40:20 -0400

    update DBSTORAGES options from settings.STORAGE_OPTIONS if the k,v pair does not already exist

--- dbbackup/storage.py
+++ dbbackup/storage.py
@@ -75,7 +75,9 @@ class Storage(object):
         """
         self._storage_path = storage_path or settings.STORAGE
         options = options.copy()
-        options.update(settings.STORAGE_OPTIONS)
+        for k, v in settings.STORAGE_OPTIONS:
+            if k not in options:
+                options[k] = v
         options = dict([(key.lower(), value) for key, value in options.items()])
         self.storageCls = get_storage_class(self._storage_path)
         self.storage = self.storageCls(**options)

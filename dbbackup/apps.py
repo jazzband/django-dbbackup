01b54b0ad19652c7869c16dbd46696dd6960750d
Frédéric Ravetier <frederic.ravetier@ametys.org>
2021-01-08 17:49:51 +0100

    Remove deprecation warning RemovedInDjango40Warning

--- dbbackup/apps.py
+++ dbbackup/apps.py
@@ -1,7 +1,10 @@
 """Apps for DBBackup"""
 
 from django.apps import AppConfig
-from django.utils.translation import gettext_lazy
+try:
+    from django.utils.translation import gettext_lazy as _
+except:
+    from django.utils.translation import ugettext_lazy as _
 
 from dbbackup import log
 

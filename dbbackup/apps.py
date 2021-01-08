8981f9fdafad6183c6277c977baa45093f62bba9
Frédéric Ravetier <frederic.ravetier@ametys.org>
2021-01-08 18:01:37 +0100

    Remove deprecation warning RemovedInDjango40Warning

--- dbbackup/apps.py
+++ dbbackup/apps.py
@@ -2,9 +2,9 @@
 
 from django.apps import AppConfig
 try:
-    from django.utils.translation import gettext_lazy as _
-except:
-    from django.utils.translation import ugettext_lazy as _
+    from django.utils.translation import gettext_lazy as _ # noqa: F401
+except: # noqa: E722
+    from django.utils.translation import ugettext_lazy as _ # noqa: F401
 
 from dbbackup import log
 
@@ -15,7 +15,7 @@ class DbbackupConfig(AppConfig):
     """
     name = 'dbbackup'
     label = 'dbbackup'
-    verbose_name = gettext_lazy('Backup and restore')
+    verbose_name = _('Backup and restore')
 
     def ready(self):
         log.load()

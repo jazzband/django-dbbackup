cac16383c3f9bc84d155c20eda55d2b97374d828
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-04-27 22:56:16 -0700

    attempt at fixing codecov hits

--- dbbackup/tests/test_connectors/test_sqlite.py
+++ dbbackup/tests/test_connectors/test_sqlite.py
@@ -30,7 +30,7 @@ class SqliteConnectorTest(TestCase):
         self.assertTrue(dump.read())
 
     def test_create_dump_with_newline(self):
-        TextModel.objects.create(field='foo\nbar')
+        TextModel.objects.create(field=f'INSERT({"foo" * 5000}\nbar WHERE baz);\n')
         connector = SqliteConnector()
         dump = connector.create_dump()
         self.assertTrue(dump.read())

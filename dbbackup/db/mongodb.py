4b590216616b8b7e530f6d7fa0bb0483f28bf4cd
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-04-29 01:37:32 -0700

    misc fixes

--- dbbackup/db/mongodb.py
+++ dbbackup/db/mongodb.py
@@ -8,46 +8,47 @@ class MongoDumpConnector(BaseCommandDBConnector):
     MongoDB connector, creates dump with ``mongodump`` and restore with
     ``mongorestore``.
     """
-    dump_cmd = 'mongodump'
-    restore_cmd = 'mongorestore'
+
+    dump_cmd = "mongodump"
+    restore_cmd = "mongorestore"
     object_check = True
     drop = True
 
     def _create_dump(self):
         cmd = f"{self.dump_cmd} --db {self.settings['NAME']}"
-        host = self.settings.get('HOST') or 'localhost'
-        port = self.settings.get('PORT') or 27017
-        cmd += f' --host {host}:{port}'
-        if self.settings.get('USER'):
+        host = self.settings.get("HOST") or "localhost"
+        port = self.settings.get("PORT") or 27017
+        cmd += f" --host {host}:{port}"
+        if self.settings.get("USER"):
             cmd += f" --username {self.settings['USER']}"
-        if self.settings.get('PASSWORD'):
+        if self.settings.get("PASSWORD"):
             cmd += f" --password {utils.get_escaped_command_arg(self.settings['PASSWORD'])}"
 
-        if self.settings.get('AUTH_SOURCE'):
+        if self.settings.get("AUTH_SOURCE"):
             cmd += f" --authenticationDatabase {self.settings['AUTH_SOURCE']}"
         for collection in self.exclude:
-            cmd += f' --excludeCollection {collection}'
-        cmd += ' --archive'
-        cmd = f'{self.dump_prefix} {cmd} {self.dump_suffix}'
-        stdout, stderr = self.run_command(cmd, env=self.dump_env)
+            cmd += f" --excludeCollection {collection}"
+        cmd += " --archive"
+        cmd = f"{self.dump_prefix} {cmd} {self.dump_suffix}"
+        stdout, _stderr = self.run_command(cmd, env=self.dump_env)
         return stdout
 
     def _restore_dump(self, dump):
         cmd = self.restore_cmd
-        host = self.settings.get('HOST') or 'localhost'
-        port = self.settings.get('PORT') or 27017
-        cmd += f' --host {host}:{port}'
-        if self.settings.get('USER'):
+        host = self.settings.get("HOST") or "localhost"
+        port = self.settings.get("PORT") or 27017
+        cmd += f" --host {host}:{port}"
+        if self.settings.get("USER"):
             cmd += f" --username {self.settings['USER']}"
-        if self.settings.get('PASSWORD'):
+        if self.settings.get("PASSWORD"):
             cmd += f" --password {utils.get_escaped_command_arg(self.settings['PASSWORD'])}"
 
-        if self.settings.get('AUTH_SOURCE'):
+        if self.settings.get("AUTH_SOURCE"):
             cmd += f" --authenticationDatabase {self.settings['AUTH_SOURCE']}"
         if self.object_check:
-            cmd += ' --objcheck'
+            cmd += " --objcheck"
         if self.drop:
-            cmd += ' --drop'
-        cmd += ' --archive'
-        cmd = f'{self.restore_prefix} {cmd} {self.restore_suffix}'
+            cmd += " --drop"
+        cmd += " --archive"
+        cmd = f"{self.restore_prefix} {cmd} {self.restore_suffix}"
         return self.run_command(cmd, stdin=dump, env=self.restore_env)
--- dbbackup/db/mysql.py
+++ dbbackup/db/mysql.py
@@ -8,37 +8,38 @@ class MysqlDumpConnector(BaseCommandDBConnector):
     MySQL connector, creates dump with ``mysqldump`` and restore with
     ``mysql``.
     """
-    dump_cmd = 'mysqldump'
-    restore_cmd = 'mysql'
+
+    dump_cmd = "mysqldump"
+    restore_cmd = "mysql"
 
     def _create_dump(self):
         cmd = f"{self.dump_cmd} {self.settings['NAME']} --quick"
-        if self.settings.get('HOST'):
+        if self.settings.get("HOST"):
             cmd += f" --host={self.settings['HOST']}"
-        if self.settings.get('PORT'):
+        if self.settings.get("PORT"):
             cmd += f" --port={self.settings['PORT']}"
-        if self.settings.get('USER'):
+        if self.settings.get("USER"):
             cmd += f" --user={self.settings['USER']}"
-        if self.settings.get('PASSWORD'):
+        if self.settings.get("PASSWORD"):
             cmd += f" --password={utils.get_escaped_command_arg(self.settings['PASSWORD'])}"
 
         for table in self.exclude:
             cmd += f" --ignore-table={self.settings['NAME']}.{table}"
-        cmd = f'{self.dump_prefix} {cmd} {self.dump_suffix}'
-        stdout, stderr = self.run_command(cmd, env=self.dump_env)
+        cmd = f"{self.dump_prefix} {cmd} {self.dump_suffix}"
+        stdout, _stderr = self.run_command(cmd, env=self.dump_env)
         return stdout
 
     def _restore_dump(self, dump):
         cmd = f"{self.restore_cmd} {self.settings['NAME']}"
-        if self.settings.get('HOST'):
+        if self.settings.get("HOST"):
             cmd += f" --host={self.settings['HOST']}"
-        if self.settings.get('PORT'):
+        if self.settings.get("PORT"):
             cmd += f" --port={self.settings['PORT']}"
-        if self.settings.get('USER'):
+        if self.settings.get("USER"):
             cmd += f" --user={self.settings['USER']}"
-        if self.settings.get('PASSWORD'):
+        if self.settings.get("PASSWORD"):
             cmd += f" --password={utils.get_escaped_command_arg(self.settings['PASSWORD'])}"
 
-        cmd = f'{self.restore_prefix} {cmd} {self.restore_suffix}'
+        cmd = f"{self.restore_prefix} {cmd} {self.restore_suffix}"
         stdout, stderr = self.run_command(cmd, stdin=dump, env=self.restore_env)
         return stdout, stderr

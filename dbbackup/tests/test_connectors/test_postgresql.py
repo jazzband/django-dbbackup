34195436bc78b0b5693549cd1971c3c9d95c9ee7
Adam Ford <adam@fordys.net>
2025-04-29 21:36:48 +0100

    fix: unit test

--- dbbackup/tests/test_connectors/test_postgresql.py
+++ dbbackup/tests/test_connectors/test_postgresql.py
@@ -212,16 +212,16 @@ class PgDumpBinaryConnectorTest(TestCase):
         self.connector.create_dump()
         self.assertNotIn(" --clean", mock_dump_cmd.call_args[0][0])
 
-    def test_create_dump_if_exists(self, mock_dump_cmd, mock_restore_cmd):
+    def test_create_dump_if_exists(self, mock_run_command):
         dump = self.connector.create_dump()
         # Without
         self.connector.if_exists = False
         self.connector.restore_dump(dump)
-        self.assertNotIn(" --if-exists", mock_restore_cmd.call_args[0][0])
+        self.assertNotIn(" --if-exists", mock_run_command.call_args[0][0])
         # With
         self.connector.if_exists = True
         self.connector.restore_dump(dump)
-        self.assertIn(" --if-exists", mock_restore_cmd.call_args[0][0])
+        self.assertIn(" --if-exists", mock_run_command.call_args[0][0])
 
     @patch(
         "dbbackup.db.postgresql.PgDumpBinaryConnector.run_command",
